b4eb9ce15fb9442eaa71b06933e32d668be1321a
Tim De Pauw <tim.depauw@zentrick.com>
2015-05-06 14:16:54 +0200

    Adding logic to install playerglobal.swc

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 'use strict'
 
+var Promise = require('es6-promise').Promise
 var promisify = require('es6-promisify')
 var Download = require('download')
 var tmp = require('tmp')
 var fs = require('fs')
 var path = require('path')
+var os = require('os')
 
 var mkdirp = promisify(require('mkdirp'))
 var rimraf = promisify(require('rimraf'))
+var installPlayerglobal = promisify(require('playerglobal-latest').install)
 var mktmpdir = promisify(tmp.dir.bind(tmp))
+var chmod = promisify(fs.chmod)
+var readdir = promisify(fs.readdir)
 var rename = promisify(fs.rename)
 var stat = promisify(fs.stat)
 
 var ROOT = path.join(__dirname, 'lib', 'flex-sdk')
 var VERSIONS = require(path.join(__dirname, 'versions.json'))
+
+var isWindows = /^win/.test(os.platform())
 
 var toUrl = function (version) {
   if (!VERSIONS.hasOwnProperty(version)) {
@@ -28,6 +35,26 @@ var toPath = function (version) {
 
 var versions = function () {
   return Object.keys(VERSIONS)
+}
+
+var maybeMakeExecutable = function (file) {
+  return stat(file)
+    .then(function (stats) {
+      if (!stats.isDirectory()) {
+        return chmod(file, '755')
+      }
+    })
+}
+
+var fixPermissions = function (dir) {
+  return readdir(dir)
+    .then(function (contents) {
+      return contents.reduce(function (prev, curr) {
+        return prev.then(function () {
+          return maybeMakeExecutable(path.join(dir, curr))
+        })
+      }, Promise.resolve())
+    })
 }
 
 var downloadAndExtract = function (url, dir) {
@@ -48,6 +75,14 @@ var download = function (version) {
     .then(function (_tmpdir) {
       tmpdir = _tmpdir
       return downloadAndExtract(url, tmpdir)
+    })
+    .then(function () {
+      if (!isWindows) {
+        return fixPermissions(path.join(tmpdir, 'bin'))
+      }
+    })
+    .then(function () {
+      return installPlayerglobal(tmpdir)
     })
     .then(function () {
       return rimraf(target)
